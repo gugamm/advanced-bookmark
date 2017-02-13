d6a80ca231173a353555a1d7fa46c2474d2b6fae
gugamm <gugamm@live.com>
2017-02-13 02:40:10 -0800

    Add build configuration

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack           = require('webpack');
+const path              = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry : './src/index.js',
+  output : {
+    path : path.resolve(__dirname, 'dist'),
+    filename : 'bundle.js'
+  },
+  module : {
+    rules : [
+      {
+        test    : /\.jsx?$/,
+        exclude : /node_modules/,
+        use     : 'babel-loader'
+      }
+    ]
+  },
+  plugins : [
+    new HtmlWebpackPlugin({
+      template : './src/index.ejs',
+      title : 'Advanced Bookmark'
+    })
+  ]
+};
+
+module.exports = config;
